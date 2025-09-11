9a1f2ad8e5b7905bca613f3febb0e4a07e385b2d
Ann K <kubareva.ann@gmail.com>
2025-09-11 18:41:25 +0300

    fixed initMongoConnection

--- src/db/initMongoConnection.js
+++ src/db/initMongoConnection.js
@@ -1,10 +1,19 @@
-// src/db/initMongoDB.js
+import { getEnvVar } from '../utils/getEnvVar.js';
+import mongoose from 'mongoose';
 
-export const initMongoDB = async () => {
+export const initMongoConnection = async () => {
+  const user = getEnvVar('MONGODB_USER');
+  const pwd = getEnvVar('MONGODB_PASSWORD');
+  const url = getEnvVar('MONGODB_URL');
+  const db = getEnvVar('MONGODB_DB');
+
+  const connectionString = `mongodb+srv://${user}:${pwd}@${url}/${db}?retryWrites=true&w=majority`;
+
   try {
-    console.log("Mongo connection successfully established!");
-  } catch (e) {
-    console.log("Error while setting up mongo connection", e);
-    throw e;
+    await mongoose.connect(connectionString);
+    console.log('MongoDB connection successful.');
+  } catch (error) {
+    console.error('MongoDB connection failed:', error.message);
+    throw error;
   }
 };
