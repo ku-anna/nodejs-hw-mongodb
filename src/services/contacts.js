71bace0dc4876ef82965b54457cd1b9cff022d33
Ann K <kubareva.ann@gmail.com>
2025-09-12 14:07:50 +0300

    ...

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import ContactsCollection from '.contacts.js';
+import { ContactsCollection } from '../models/contacts.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
