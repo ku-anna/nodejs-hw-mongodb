e3bebe8ca46c2ceb29c8af5f9b923a014db00ae9
Ann K <kubareva.ann@gmail.com>
2025-09-12 10:18:31 +0300

    fixed imports

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
-import { getAllContacts, getContactById } from './services/contacts';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = process.env.PORT || 3000;
 
