d2733bab29451198718db3a50916803067d9013a
Ann K <kubareva.ann@gmail.com>
2025-09-12 13:58:31 +0300

    fixed structure and import

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,4 +1,4 @@
-import ContactsCollection from '../db/models/contacts.js';
+import ContactsCollection from './models/contacts.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
