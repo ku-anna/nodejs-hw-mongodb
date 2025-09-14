cb6f32338427c42cfa8a80782d193f62e892a792
Ann K <kubareva.ann@gmail.com>
2025-09-14 21:11:56 +0300

    fixed hw

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,10 +2,24 @@ import { ContactsCollection } from '../models/contacts.js';
 
 export const getAllContacts = async () => {
   const contacts = await ContactsCollection.find();
-  return contacts;
+
+  return {
+    status: 200,
+    message: 'Successfully found contacts!',
+    data: contacts,
+  };
 };
 
 export const getContactById = async (contactId) => {
   const contact = await ContactsCollection.findById(contactId);
-  return contact;
+
+  if (!contact) {
+    return null;
+  }
+
+  return {
+    status: 200,
+    message: `Successfully found contact with id ${contactId}!`,
+    data: contact,
+  };
 };
