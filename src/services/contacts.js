f4d939947f1f9ad76c5fb544b8898202d8cffacb
Ann K <kubareva.ann@gmail.com>
2025-10-01 15:31:18 +0300

    jesus help me

--- src/services/contacts.js
+++ src/services/contacts.js
@@ -2,7 +2,7 @@ import { ContactsCollection } from '../models/contacts.js';
 import { SORT_ORDER } from '../constants/index.js';
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
-import { User } from '.user.js';
+import { User } from '../models/user.js';
 
 const SALT_ROUNDS = 10;
 
