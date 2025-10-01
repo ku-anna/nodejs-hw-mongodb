fcd1a9e7a7acfa066efb130185200bb1b3039e62
Ann K <kubareva.ann@gmail.com>
2025-10-01 21:53:38 +0300

    fixed server.js

--- src/server.js
+++ src/server.js
@@ -3,7 +3,12 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
 import contactsRouter from './routers/contacts.js';
+import authRouter from './routers/auth.js';
+import router from './routers/index.js';
+
 import { getEnvVar } from './utils/getEnvVar.js';
 import { errorHandler } from './middlewares/errorHandler.js';
 import { notFoundHandler } from './middlewares/notFoundHandler.js';
@@ -13,23 +18,37 @@ const PORT = Number(getEnvVar('PORT', '3000'));
 export const setupServer = () => {
   const app = express();
 
-  app.use(express.json());
-  app.use(cors());
+  app.set('trust proxy', 1);
 
-  app.use('/contacts', contactsRouter);
-
-  // logging
   app.use(
-    pino({
-      transport: {
-        target: 'pino-pretty',
-      },
+    express.json({
+      type: ['application/json', 'application/vnd.api+json'],
+      limit: '100kb',
     }),
   );
-  //404
-  app.use(notFoundHandler);
+  app.use(
+    cors({
+      origin: true,
+      credentials: true,
+    }),
+  );
+  app.use(cookieParser());
+  app.use(
+    pino({
+      transport: { target: 'pino-pretty' },
+    }),
+  );
 
-  // error
+  app.get('/', (_req, res) => {
+    res.json({ status: 200, message: 'API up' });
+  });
+
+  app.use('/auth', authRouter);
+  app.use('/contacts', contactsRouter);
+
+  app.use(router);
+
+  app.use(notFoundHandler);
   app.use(errorHandler);
 
   app.listen(PORT, () => {
