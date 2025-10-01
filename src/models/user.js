84fc3aa112b7b45e8fdddfe6359748f700b0672f
Ann K <kubareva.ann@gmail.com>
2025-10-01 13:24:24 +0300

    tasks 1 and 2

---
+++ src/models/user.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: true,
+      lowercase: true,
+      match: [/^\S+@\S+\.\S+$/, 'Invalid email format'],
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required'],
+      minlength: 6,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
+
+export const User = mongoose.model('User', userSchema);
